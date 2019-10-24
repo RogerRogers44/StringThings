cdbadd53f7c70761343cab182c6bc213a3af354d
100168747 <whatsitsname123@gmail.com>
2019-10-24 15:27:36 -0500

    Break time

--- Strings and Things.py
+++ Strings and Things.py
@@ -8,3 +8,21 @@ firstName = "Wilma"
 lastName = "Flint"
 
 print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+
+print("Hip"*10 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + " your boat")
+    print("Gently down the stream")
+    print("Merrily"*4)
+    print("Life is but a dream")
+
+
+rowYourBoat()

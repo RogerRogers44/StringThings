dbb0d6d32660391d44b08115b3bcf3b8e4d02b04
100168747 <whatsitsname123@gmail.com>
2019-10-24 15:38:14 -0500

    Break time

--- Strings and Things.py
+++ Strings and Things.py
@@ -19,9 +19,9 @@ print(lastFirst)
 print("Hip"*10 + "Hooray!")
 
 def rowYourBoat():
-    print("Row, "*3 + " your boat")
+    print("Row, "*3 + "your boat")
     print("Gently down the stream")
-    print("Merrily"*4)
+    print("Merrily "*4)
     print("Life is but a dream")
 
 
@@ -30,4 +30,11 @@ rowYourBoat()
 # Indexing
 
 name = "Roy G Biv"
+firstCharacter = name[0]
+print(firstCharacter)
+
+middleCharIndex = len(name) // 2
+print(middleCharIndex)
+print(name[middleCharIndex])
+print(name[-1])
 
